caaaf8cdfd25f27c3352ddf01660e277c07b2f63
DESKTOP-O6VHCR8\ruiy~~ <2997215859@qq.com>
2017-12-23 21:39:29 +0800

    对请求做分开处理，已经完成请求行和请求头的处理

--- WebServer/server_base.h
+++ WebServer/server_base.h
@@ -9,6 +9,7 @@
 #include<functional>
 #include<boost/asio.hpp>
 #include<thread>
+#include<regex>
 
 
 namespace WebServer{
@@ -27,15 +28,73 @@ namespace WebServer{
 		resource_type resources;
 		resource_type default_resources;
 
-		void start();
+		void start() {
+			for (auto it = resources.begin(); it != resources.end();it++) {
+				all_resources.push_back(it);
+			}
+			for (auto it = default_resources.begin(); it != default_resources.end(); it++) {
+				all_resources.push_back(it);
+			}
+			accept();
+			for (size_t i = 1; i < nums_threads;i++) {
+				threads.emplace_back([this]() {
+					m_io_service.run();
+				});
+			}
+			// 主线程
+			m_io_service.run();
+			for (auto& t: threads) {
+				t.join();
+			}
+		};
 	protected:
 		boost::asio::io_service m_io_service;
 		boost::asio::ip::tcp::acceptor acceptor;
 		boost::asio::ip::tcp::endpoint endpoint;
 		size_t num_threads;
-		std::vector<std::thread> threads();
+		std::vector<std::thread> threads;
+		std::vector<resource_type::iterator> all_resources;
 		virtual void accept();
-		void processRequestAndResponse(std::shared_ptr<socket_type> socket);
+		void processRequestAndResponse(std::shared_ptr<socket_type> socket) const {
+			auto read_buffer = std::make_shared<boost::asio::streambuf>();
+			boost::asio::async_read_until(
+				*socket,
+				*read_buffer, 
+				'\r\n\r\n',
+				[this, socket, read_buffer](const boost::system::error_code& ec, size_t bytes_transferred){
+				if (!ec){
+					size_t total = read_buffer->size();
+					std::istream stream(read_buffer.get());
+					auto request = std::make_shared<Request>();
+					// 这里的设计可能并不好，应该要求返回一个共享指针，以避免程序员可能的错误使用
+					reqeust = parse_request()
+				}
+			});
+		};
+		Request parse_request(std::istream& istream) const {
+			Request request;
+			std::regex re("^([^ ]*) ([^ ]*) HTTP/([^ ]*)$");
+			std::smatch sub_match;
+			std::string line;
+			getline(istream, line);
+			line.pop_back();
+			if (std::regex_match(line, sub_match, re)) {
+				request.method = sub_match[1];
+				request.path = sub_match[2];
+				request.http_version = sub_match[3];
+				bool matched;
+				re = "^([^:]*): ?(.*)$";
+				do {
+					getline(istream, line);
+					line.pop_back();
+					matched = std::regex_match(line, sub_match, re);
+					if (matched) {
+						request.headers[sub_match[1]] = sub_match[2];
+					}
+				} while(matched==true)
+			}
+			return request;
+		}
 	};
 
 	template<typename socket_type>
